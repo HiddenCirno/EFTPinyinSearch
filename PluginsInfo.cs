e9b737899e3a75ba2e54106ea3f7a869a2cb6dc0
HiddenCirno <2301697863@qq.com>
2026-08-07 23:17:31 +0800

    版本号

--- PluginsInfo.cs
+++ PluginsInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "hiddenhiragi.sparkproject.pinyinsearch";
         public const string NAME = "星火计划: 拼音搜索";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.1.0";
     }
 }
